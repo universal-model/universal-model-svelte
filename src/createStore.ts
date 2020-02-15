cd2e719db47a22c6836125de9cc503fef97f0656
Petri Silen <petri.silen@silensoft.com>
2020-02-15 12:43:02 +0200

    Added tests

--- src/createStore.ts
+++ src/createStore.ts
@@ -1,8 +1,16 @@
-import Store, { Selectors, SelectorsBase, State } from './Store';
+import Store, { Selectors, SelectorsBase, State, SubState } from './Store';
 
 export default function createStore<T extends State, U extends SelectorsBase<T>>(
   initialState: T,
   selectors: Selectors<T, U>
 ): Store<T, U> {
+  Object.entries(initialState).forEach(([, subState]: [string, SubState]) => {
+    if (!subState.__isSubState__) {
+      throw new Error(
+        'createStore: One of given subStates is not subState. You must call createSubState(subState)'
+      );
+    }
+  });
+
   return new Store(initialState, selectors);
 }
